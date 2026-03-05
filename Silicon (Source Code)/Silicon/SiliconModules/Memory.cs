e0eea2055ad05741f7ebecd3529e6d3bd25335c0
TreacherousDev <treacherousdev@gmail.com>
2026-03-05 21:27:35 +0800

    why did cubic castles had to update 47 microseconds after i made a new release
    im about to turn into a statistic if this doesnt last at least another month

--- Silicon (Source Code)/Silicon/SiliconModules/Memory.cs
+++ Silicon (Source Code)/Silicon/SiliconModules/Memory.cs
@@ -17,40 +17,40 @@ namespace Silicon
         readonly string cameraCoordinatesFunction = "90 90 90 90 90 90 90 90 90 90 90 90";
         readonly string adjustFogFunction = "90 90 90 90 90 90 90 90";
 
-        readonly string cameraHeightInjection = "53 E8 00 00 00 00 5B F3 0F 5C 43 1B F3 0F 11 40 08 5B F3 0F 5C CB 8D 85 FC FE FF FF E9 89 F0 EA FF 66 66 A6 3F";
-        readonly string cameraHeightFunctionEntry = "E9 5B 0F 15 00 0F 1F 44 00 00";
+        readonly string cameraHeightInjection = "53 E8 00 00 00 00 5B F3 0F 5C 43 1B F3 0F 11 40 08 5B F3 0F 5C CB 8D 85 FC FE FF FF E9 33 F9 EA FF 66 66 A6 3F 00 00 00";
+        readonly string cameraHeightFunctionEntry = "E9 B1 06 15 00 0F 1F 44 00 00";
 
-        readonly string unlockCameraArrowsInjection = "50 E8 00 00 00 00 58 F3 0F 10 58 1F 0F 2F D8 58 0F 86 C2 B3 EA FF C7 86 EC 09 00 00 00 00 B2 C2 E9 B3 B3 EA FF 00 00 B2 C2 00 00 00 00";
-        readonly string unlockCameraArrowsFunctionEntry = "E9 2F 4C 15 00";
+        readonly string unlockCameraArrowsInjection = "50 E8 00 00 00 00 58 F3 0F 10 58 1F 0F 2F D8 58 0F 86 4B BC EA FF C7 86 EC 09 00 00 00 00 B2 C2 E9 3C BC EA FF 00 00 B2 C2 00 00 00";
+        readonly string unlockCameraArrowsFunctionEntry = "E9 A6 43 15 00";
 
-        readonly string unlockCameraRMBInjection = "50 E8 00 00 00 00 58 F3 0F 10 70 1F 0F 2F F1 58 0F 86 6C CA EB FF C7 80 EC 09 00 00 00 00 B2 C2 E9 5D CA EB FF 00 00 B2 C2 00 00 00 00";
-        readonly string unlockCameraRMBFunctionEntry = "E9 85 35 14 00";
+        readonly string unlockCameraRMBInjection = "50 E8 00 00 00 00 58 F3 0F 10 70 1F 0F 2F F1 58 0F 86 3F D4 EB FF C7 80 EC 09 00 00 00 00 B2 C2 E9 30 D4 EB FF 00 00 B2 C2 00 00 01 00 00 00 00";
+        readonly string unlockCameraRMBFunctionEntry = "E9 AF 2B 14 00";
 
-        readonly string unlockCameraFOVInjection = "50 E8 00 00 00 00 58 F3 0F 10 40 12 58 8D 85 D8 FE FF FF E9 EE EF EA FF 00 00 96 42 FF FF FF FF";
-        readonly string unlockCameraFOVFunctionEntry = "E9 FB 0F 15 00 90";
+        readonly string unlockCameraFOVInjection = "50 E8 00 00 00 00 58 F3 0F 10 40 12 58 8D 85 D8 FE FF FF E9 98 F8 EA FF 00 00 96 42 FF FF FF FF 00 00 00 00";
+        readonly string unlockCameraFOVFunctionEntry = "E9 51 07 15 00 90";
 
-        readonly string adjustCameraDistanceInjection = "50 E8 00 00 00 00 58 F3 0F 59 40 24 F3 0F 59 58 24 F3 0F 59 60 24 58 E9 00 00 00 00 F3 0F 5C D0 F3 0F 10 40 08 E9 25 EE EA FF 00 00 C8 41 FF FF FF FF";
-        readonly string adjustCameraDistanceFunctionEntry = "E9 B5 11 15 00 0F 1F 40 00";
+        readonly string adjustCameraDistanceInjection = "50 E8 00 00 00 00 58 F3 0F 59 40 24 F3 0F 59 58 24 F3 0F 59 60 24 58 E9 00 00 00 00 F3 0F 5C D0 F3 0F 10 40 08 E9 CF F6 EA FF 00 00 C8 41 FF FF FF FF 00 00 00 00";
+        readonly string adjustCameraDistanceFunctionEntry = "E9 0B 09 15 00 0F 1F 40 00";
 
-        readonly string upVectorInjection = "55 8B EC 8B 55 08 39 D1 0F 84 B4 92 D6 FF 50 E8 00 00 00 00 58 53 8B 58 2B 89 1A 8B 58 2F 89 5A 04 8B 58 33 89 5A 08 5B 58 8B 02 89 01 8B 42 04 89 41 04 8B 42 08 89 41 08 8B C1 5D C2 04 00 00 00 00 00 00 00 00 00 00 00 80 BF FF FF FF FF";
-        readonly string upVectorFunctionEntry = "E8 63 12 15 00";
+        readonly string upVectorInjection = "55 8B EC 8B 55 08 39 D1 0F 84 9A 92 D6 FF 50 E8 00 00 00 00 58 53 8B 58 2B 89 1A 8B 58 2F 89 5A 04 8B 58 33 89 5A 08 5B 58 8B 02 89 01 8B 42 04 89 41 04 8B 42 08 89 41 08 8B C1 5D C2 04 00 00 00 00 00 00 00 00 00 00 00 80 BF FF FF FF FF";
+        readonly string upVectorFunctionEntry = "E8 B9 09 15 00";
 
         readonly string overrideArrowHotkeysFunction = "90 90 90 90 90 90 90 90";
         readonly string overrideRightClickDragFunction = "90 90 90 90 90 90 90 90 90 90 90 90 90 90 90 90";
 
-        readonly string closeChatInjection = "50 E8 00 00 00 00 58 C7 40 14 00 00 00 00 58 FF 92 A4 00 00 00 E9 BE D7 DA FF 00 00 00 00 FF FF FF FF";
-        readonly string closeChatFunctionEntry = "E9 29 28 25 00 90";
+        readonly string closeChatInjection = "50 E8 00 00 00 00 58 C7 40 14 00 00 00 00 58 FF 92 A4 00 00 00 E9 2E D9 DA FF 00 00 00 00 FF FF FF FF";
+        readonly string closeChatFunctionEntry = "E9 B9 26 25 00 90";
 
-        readonly string openChatInjection = "50 E8 00 00 00 00 58 C7 40 D8 01 00 00 00 58 83 F0 02 89 46 64 E9 68 D7 DA FF 00 00 00 00";
-        readonly string openChatFunctionEntry = "E9 7F 28 25 00 90";
+        readonly string openChatInjection = "50 E8 00 00 00 00 58 C7 40 D8 01 00 00 00 58 83 F0 02 89 46 64 E9 D8 D8 DA FF 00 80 05 6C 00 00 00 00";
+        readonly string openChatFunctionEntry = "E9 0F 27 25 00 90";
 
         // Revertable functions (Optional switch states available)
-        readonly string cameraLookAtEditorInjection = "50 E8 00 00 00 00 58 F3 0F 11 58 5D F3 0F 11 48 61 F3 0F 11 40 65 F3 0F 10 58 4D F3 0F 10 48 51 F3 0F 10 40 55 58 50 E8 00 00 00 00 58 F3 0F 11 58 37 F3 0F 11 48 3B F3 0F 11 40 3F 53 8D 5E 10 89 58 33 5B 58 F3 0F 11 1E F3 0F 11 4E 04 E9 6F B6 EA FF 00 00 00 00 00 00 00 00 00 00 8C 42 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 FF FF FF FF";
-        readonly string cameraLookAtEditorFunctionEntry = "E9 42 49 15 00 0F 1F 40 00";
-        readonly string cameraLookAtEditorFunctionOriginal = "E9 68 49 15 00 0F 1F 40 00";
+        readonly string cameraLookAtEditorInjection = "50 E8 00 00 00 00 58 F3 0F 11 48 5D F3 0F 11 50 61 F3 0F 11 40 65 F3 0F 10 48 4D F3 0F 10 50 51 F3 0F 10 40 55 58 50 E8 00 00 00 00 58 F3 0F 11 48 37 F3 0F 11 50 3B F3 0F 11 40 3F 53 8D 5E 10 89 58 33 5B 58 F3 0F 11 0E F3 0F 11 56 04 E9 F8 BE EA FF 00 00 00 00 00 00 00 00 00 00 8C 42 18 B4 8C 10 00 00 00 00 00 00 00 00 00 00 8C 42 FF FF FF FF";
+        readonly string cameraLookAtEditorFunctionEntry = "E9 B9 40 15 00 0F 1F 40 00";
+        readonly string cameraLookAtEditorFunctionOriginal = "E9 DF 40 15 00 0F 1F 40 00";
 
-        readonly string hidePlayerAvatarInjection = "53 E8 00 00 00 00 5B F3 0F 10 7D 08 F3 0F 5C BB 65 01 00 00 50 F3 0F 11 7B 6B 8B 43 6B 23 43 63 66 0F 6E F8 58 0F 2E 7B 67 0F 83 2F 00 00 00 F3 0F 10 7D 0C F3 0F 5C BB 69 01 00 00 50 F3 0F 11 7B 6B 8B 43 6B 23 43 63 66 0F 6E F8 58 0F 2E 7B 67 0F 83 07 00 00 00 C7 45 10 00 00 C8 C2 5B F3 0F 10 45 10 E9 70 20 E9 FF FF FF FF 7F 9A 99 99 3E 00 00 00 00 AA AA AA AA";
-        readonly string hidePlayerAvatarFunctionEntry = "E9 27 DF 16 00";
+        readonly string hidePlayerAvatarInjection = "53 E8 00 00 00 00 5B F3 0F 10 7D 08 F3 0F 5C BB 65 01 00 00 50 F3 0F 11 7B 6B 8B 43 6B 23 43 63 66 0F 6E F8 58 0F 2E 7B 67 0F 83 2F 00 00 00 F3 0F 10 7D 0C F3 0F 5C BB 69 01 00 00 50 F3 0F 11 7B 6B 8B 43 6B 23 43 63 66 0F 6E F8 58 0F 2E 7B 67 0F 83 07 00 00 00 C7 45 10 00 00 C8 C2 5B F3 0F 10 45 10 E9 FB 28 E9 FF FF FF FF 7F 9A 99 99 3E 00 00 00 00 AA AA AA AA";
+        readonly string hidePlayerAvatarFunctionEntry = "E9 9C D6 16 00";
         readonly string hidePlayerAvatarFunctionOriginal = "F3 0F 10 45 10";
 
         readonly string disableMovement1Function = "90 90 90 90";
@@ -58,8 +58,8 @@ namespace Silicon
         readonly string disableMovement2Function = "90 90 90 90 90 90 90 90";
         readonly string disableMovement2Original = "F3 0F 11 87 80 00 00 00";
 
-        readonly string itemBobbingInjection = "50 E8 00 00 00 00 58 F3 0F 10 40 29 F3 0F 11 87 A4 01 00 00 F3 0F 10 87 A8 01 00 00 F3 0F 10 40 2D F3 0F 11 87 A8 01 00 00 58 E9 93 C8 E9 FF 00 00 00 00";
-        readonly string itemBobbingFunctionEntry = "E9 55 37 16 00 0F 1F";
+        readonly string itemBobbingInjection = "50 E8 00 00 00 00 58 F3 0F 10 40 29 F3 0F 11 87 A4 01 00 00 F3 0F 10 87 A8 01 00 00 F3 0F 10 40 2D F3 0F 11 87 A8 01 00 00 58 E9 56 D2 E9 FF 00 00 00 00";
+        readonly string itemBobbingFunctionEntry = "E9 C8 2E 16 00 0F 1F";
         readonly string itemBobbingFunctionOriginal= "F3 0F 11 87 A4 01 00";
 
         //
@@ -127,34 +127,34 @@ namespace Silicon
         {
             UnlockCubicMemoryRegion();
 
-            m.WriteMemory("Cubic.exe+1BD80A", "bytes", cameraCoordinatesFunction);
-            m.WriteMemory("Cubic.exe+1BD921", "bytes", adjustFogFunction);
+            m.WriteMemory("Cubic.exe+1BE0B4", "bytes", cameraCoordinatesFunction);
+            m.WriteMemory("Cubic.exe+1BE1CB", "bytes", adjustFogFunction);
 
             // Special case for cameraLookAtEditor
             // Always inject, but skip assignment if deactivated (inject to the adress after assignment)
             m.WriteMemory("Cubic.exe+30EA2D", "bytes", cameraLookAtEditorInjection);
-            m.WriteMemory("Cubic.exe+1BA0E6", "bytes", cameraLookAtEditorFunctionOriginal);
+            m.WriteMemory("Cubic.exe+1BA96F", "bytes", cameraLookAtEditorFunctionOriginal);
 
             m.WriteMemory("Cubic.exe+30E77F", "bytes", cameraHeightInjection);
-            m.WriteMemory("Cubic.exe+1BD81F", "bytes", cameraHeightFunctionEntry);
+            m.WriteMemory("Cubic.exe+1BE0C9", "bytes", cameraHeightFunctionEntry);
             m.WriteMemory("Cubic.exe+30E7C8", "bytes", unlockCameraArrowsInjection);
-            m.WriteMemory("Cubic.exe+1B9B94", "bytes", unlockCameraArrowsFunctionEntry);
+            m.WriteMemory("Cubic.exe+1BA41D", "bytes", unlockCameraArrowsFunctionEntry);
             m.WriteMemory("Cubic.exe+30E816", "bytes", unlockCameraRMBInjection);
-            m.WriteMemory("Cubic.exe+1CB28C", "bytes", unlockCameraRMBFunctionEntry);
+            m.WriteMemory("Cubic.exe+1CBC62", "bytes", unlockCameraRMBFunctionEntry);
             m.WriteMemory("Cubic.exe+30E853", "bytes", unlockCameraFOVInjection);
-            m.WriteMemory("Cubic.exe+1BD853", "bytes", unlockCameraFOVFunctionEntry);
+            m.WriteMemory("Cubic.exe+1BE0FD", "bytes", unlockCameraFOVFunctionEntry);
             m.WriteMemory("Cubic.exe+30E9D0", "bytes", adjustCameraDistanceInjection);
-            m.WriteMemory("Cubic.exe+1BD816", "bytes", adjustCameraDistanceFunctionEntry);
+            m.WriteMemory("Cubic.exe+1BE0C0", "bytes", adjustCameraDistanceFunctionEntry);
             m.WriteMemory("Cubic.exe+30EAE5", "bytes", upVectorInjection);
-            m.WriteMemory("Cubic.exe+1BD87D", "bytes", upVectorFunctionEntry);
+            m.WriteMemory("Cubic.exe+1BE127", "bytes", upVectorFunctionEntry);
             m.WriteMemory("Cubic.exe+30EB75", "bytes", closeChatInjection);
-            m.WriteMemory("Cubic.exe+BC347", "bytes", closeChatFunctionEntry);
+            m.WriteMemory("Cubic.exe+BC4B7", "bytes", closeChatFunctionEntry);
             m.WriteMemory("Cubic.exe+30EBB1", "bytes", openChatInjection);
-            m.WriteMemory("Cubic.exe+BC32D", "bytes", openChatFunctionEntry);
+            m.WriteMemory("Cubic.exe+BC49D", "bytes", openChatFunctionEntry);
 
-            m.WriteMemory("Cubic.exe+1B9ADE", "bytes", overrideArrowHotkeysFunction);
-            m.WriteMemory("Cubic.exe+1B9B1A", "bytes", overrideArrowHotkeysFunction);
-            m.WriteMemory("Cubic.exe+1CB22A", "bytes", overrideRightClickDragFunction);
+            m.WriteMemory("Cubic.exe+1BA367", "bytes", overrideArrowHotkeysFunction);
+            m.WriteMemory("Cubic.exe+1BA3A3", "bytes", overrideArrowHotkeysFunction);
+            m.WriteMemory("Cubic.exe+1CBBFD", "bytes", overrideRightClickDragFunction);
 
 
             //Revertable, injections only as set to false by default
@@ -223,7 +223,7 @@ namespace Silicon
             // Distance 
             m.WriteMemory("Cubic.exe+30E9FA", "bytes", ConvertDoubleToFloatBytes(currentCameraDistance));
             // Game Fog
-            m.WriteMemory("Cubic.exe+300ED0", "bytes", ConvertDoubleToFloatBytes(currentCameraSightRange));
+            m.WriteMemory("Cubic.exe+301F58", "bytes", ConvertDoubleToFloatBytes(currentCameraSightRange));
 
             if (FreecamSwitch.Switched == false)
             {
@@ -264,19 +264,19 @@ namespace Silicon
             if (enabled)
             {
                 // Start Freecam camera position and rotation at the current camera position and rotation
-                m.WriteMemory("Cubic.exe+1BA0E6", "bytes", cameraLookAtEditorFunctionEntry);
-                m.WriteMemory("Cubic.exe+21BE4B", "bytes", disableMovement1Function);
-                m.WriteMemory("Cubic.exe+21BE7A", "bytes", disableMovement1Function);
-                m.WriteMemory("Cubic.exe+21BDE1", "bytes", disableMovement2Function);
-                m.WriteMemory("Cubic.exe+21BE18", "bytes", disableMovement2Function);
+                m.WriteMemory("Cubic.exe+1BA96F", "bytes", cameraLookAtEditorFunctionEntry);
+                m.WriteMemory("Cubic.exe+21CD3B", "bytes", disableMovement1Function);
+                m.WriteMemory("Cubic.exe+21CD6A", "bytes", disableMovement1Function);
+                m.WriteMemory("Cubic.exe+21CCD1", "bytes", disableMovement2Function);
+                m.WriteMemory("Cubic.exe+21CD08", "bytes", disableMovement2Function);
             }
             else
             {
-                m.WriteMemory("Cubic.exe+1BA0E6", "bytes", cameraLookAtEditorFunctionOriginal);
-                m.WriteMemory("Cubic.exe+21BE4B", "bytes", disableMovement1Original);
-                m.WriteMemory("Cubic.exe+21BE7A", "bytes", disableMovement1Original);
-                m.WriteMemory("Cubic.exe+21BDE1", "bytes", disableMovement2Original);
-                m.WriteMemory("Cubic.exe+21BE18", "bytes", disableMovement2Original);
+                m.WriteMemory("Cubic.exe+1BA96F", "bytes", cameraLookAtEditorFunctionOriginal);
+                m.WriteMemory("Cubic.exe+21CD3B", "bytes", disableMovement1Original);
+                m.WriteMemory("Cubic.exe+21CD6A", "bytes", disableMovement1Original);
+                m.WriteMemory("Cubic.exe+21CCD1", "bytes", disableMovement2Original);
+                m.WriteMemory("Cubic.exe+21CD08", "bytes", disableMovement2Original);
                 StopAnimation();
             }
 
@@ -287,11 +287,11 @@ namespace Silicon
         {
             if (enabled)
             {
-                m.WriteMemory("Cubic.exe+1A09F9", "bytes", hidePlayerAvatarFunctionEntry);
+                m.WriteMemory("Cubic.exe+1A1284", "bytes", hidePlayerAvatarFunctionEntry);
             }
             else
             {
-                m.WriteMemory("Cubic.exe+1A09F9", "bytes", hidePlayerAvatarFunctionOriginal);
+                m.WriteMemory("Cubic.exe+1A1284", "bytes", hidePlayerAvatarFunctionOriginal);
             }
         }
 
@@ -299,13 +299,13 @@ namespace Silicon
         {
             if (enabled)
             {
-                m.WriteMemory("Cubic.exe+1BFB65", "bytes", "84");
-                m.WriteMemory("Cubic.exe+231C0F", "bytes", "C7 82 38 02 00 00 00 40 1C C6 90 90 90 90 90 90");
+                m.WriteMemory("Cubic.exe+1C040D", "bytes", "84");
+                m.WriteMemory("Cubic.exe+232AFF", "bytes", "C7 82 38 02 00 00 00 40 1C C6 90 90 90 90 90 90");
             }
             else
             {
-                m.WriteMemory("Cubic.exe+1BFB65", "bytes", "85");
-                m.WriteMemory("Cubic.exe+231C0F", "bytes", "F3 0F 11 82 38 02 00 00 F3 0F 58 8A 3C 02 00 00");
+                m.WriteMemory("Cubic.exe+1C040D", "bytes", "85");
+                m.WriteMemory("Cubic.exe+232AFF", "bytes", "F3 0F 11 82 38 02 00 00 F3 0F 58 8A 3C 02 00 00");
             }
         }
 
@@ -313,11 +313,11 @@ namespace Silicon
         {
             if (enabled)
             {
-                m.WriteMemory("Cubic.exe+1A8ADD", "bytes", "E9 09 0F 00 00 90");
+                m.WriteMemory("Cubic.exe+1A936A", "bytes", "E9 09 0F 00 00 90");
             }
             else
             {
-                m.WriteMemory("Cubic.exe+1A8ADD", "bytes", "0F 85 08 0F 00 00");
+                m.WriteMemory("Cubic.exe+1A936A", "bytes", "0F 85 08 0F 00 00");
             }   
         }
 
@@ -325,11 +325,11 @@ namespace Silicon
         {
             if (!enabled)
             {
-                m.WriteMemory("Cubic.exe+1AB4A7", "bytes", "F3 0F 11 87 A4 01 00");
+                m.WriteMemory("Cubic.exe+1ABD34", "bytes", "F3 0F 11 87 A4 01 00");
             }
             else
             {
-                m.WriteMemory("Cubic.exe+1AB4A7", "bytes", "E9 55 37 16 00 0F 1F");
+                m.WriteMemory("Cubic.exe+1ABD34", "bytes", "E9 C8 2E 16 00 0F 1F");
             }
         }
     }
